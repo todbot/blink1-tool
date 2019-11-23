8921f6d5b0c5a0459158e6dc5218184fe50319ae
Tod E. Kurt <tod@todbot.com>
2019-11-23 10:56:26 -0800

    add pattern ability to blink1-tiny-server, does not work quite yet

--- server/blink1-tiny-server.c
+++ server/blink1-tiny-server.c
@@ -23,6 +23,10 @@
 
 #include "blink1-lib.h"
 
+//#include "dict.h"
+#include "Dictionary.h"
+#include "Dictionary.c"
+
 // normally this is obtained from git tags and filled out by the Makefile
 #ifndef BLINK1_VERSION
 #define BLINK1_VERSION "v0.0"
@@ -34,7 +38,10 @@ const char* blink1_server_version = BLINK1_VERSION;
 static const char *s_http_port = "8000";
 static struct mg_serve_http_opts s_http_server_opts;
 
-typedef struct Url_Info
+DictionaryRef       patterndict;
+DictionaryCallbacks patterndictc;
+        
+typedef struct _url_info
 {
     char url[100];
     char desc[100];
@@ -44,12 +51,14 @@ typedef struct Url_Info
 url_info supported_urls[]
 = {
    {"/blink1/",      "simple status page"},
+   {"/blink1/id",    "get blink1 serial number"},
    {"/blink1/on",    "turn blink(1) full bright white"},
    {"/blink1/red",   "turn blink(1) solid red"},
    {"/blink1/green", "turn blink(1) solid green"},
    {"/blink1/blue",  "turn blink(1) solid blue"},
    {"/blink1/fadeToRGB", "turn blink(1) specified RGB color"},
    {"/blink/blink",  "blink the blink(1) the specified RGB color"},
+   {"/blink/pattern",  "play color pattern specified by 'pattern' arg"},
    {"/blink/random",  "turn the blink(1) a random color"}
 };
 
@@ -81,29 +90,62 @@ void usage()
 "  'ledn'   -- which LED to set. 0=all/1=top/2=bot, e.g. 'ledn=0'\n"
 "  'milils' -- milliseconds to fade, or blink, e.g. 'millis=500'\n"
 "  'count'  -- number of times to blink, for /blink1/blink, e.g. 'count=3'\n"
+"  'pattern'-- color pattern string (e.g. '3,#00ffff,0.2,0,#000000,0.2,0')\n"
 "\n"
 "Examples: \n"
 "  /blink1/blue?bright=127 -- set blink1 blue, at half-intensity \n"
 "  /blink1/fadeToRGB?rgb=%%23FF00FF&millis=500 -- fade to purple over 500ms\n"
+"  /blink1/blink1/pattern?pattern=3,%%2300ffff,0.2,0,%%23000000,0.2,0 -- blink teal 3 times\n"
             
 "\n"
             
             );
 }
 
-// used in ev_handler below
-// uses variables bright,r,g,b,ledn, result
-#define do_blink1_color() \
-    blink1_device* dev = blink1_open(); \
-    blink1_adjustBrightness( bright, &r, &g, &b); \
-    if( blink1_fadeToRGBN( dev, millis, r,g,b, ledn ) == -1 ) {   \
-        fprintf(stderr, "fadeToRGBN: error blink1 device error\n"); \
-        sprintf(result, "%s: error, couldn't find blink1", result); \
-    } \
-    else { \
-        sprintf(result, "blink1 set color #%2.2x%2.2x%2.2x", r,g,b);  \
-    } \
-    blink1_close(dev); 
+void blink1_do_color(rgb_t rgb, uint32_t millis,
+                    uint8_t ledn, uint8_t bright, char* result)
+{
+    blink1_device* dev = blink1_open();
+    if( !dev ) {
+        sprintf(result, "%s: error: no blink1 found", result);
+        return;
+    }
+    blink1_adjustBrightness( bright, &rgb.r, &rgb.g, &rgb.b); 
+    if( millis==0 ) { millis = 200; } 
+    int rc = blink1_fadeToRGBN( dev, millis, rgb.r,rgb.g,rgb.b, ledn );
+    if( rc == -1 ) {
+        fprintf(stderr, "error, couldn't fadeToRGB on blink1\n");
+        sprintf(result, "%s: error, couldn't fadeToRGB on blink1", result);
+    } 
+    else {
+        sprintf(result, "blink1 set color #%2.2x%2.2x%2.2x", rgb.r,rgb.g,rgb.b);
+    }
+    blink1_close(dev);
+}
+
+//
+// 
+void DictionaryPrintAsJsonMg(struct mg_connection *nc, DictionaryRef d )
+{
+    size_t                  i;
+    struct DictionaryItem * item;
+
+    if( d == NULL || d->items == NULL ) { return; }
+
+    for( i = 0; i < d->size; i++ ) {
+        item = d->items[ i ];
+        
+        while( item ) {
+            const char* v = item->value;
+            if( v[0] == '[' || v[0] == '{' ) { // hack if value is json array/object 
+                mg_printf_http_chunk(nc,"\"%s\": %s,\n", item->key,item->value);
+            } else { 
+                mg_printf_http_chunk(nc,"\"%s\": \"%s\",\n", item->key,item->value);
+            }
+            item = item->next;
+        }
+    }
+}
 
 
 static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
@@ -114,22 +156,29 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
         return;
     }
 
-    uint8_t r,g,b;
     uint8_t ledn=0, bright=0;
-    char result[1000];
-    result[0] = 0;
+    char result[1000];  result[0] = 0;
     char uristr[1000];
     char tmpstr[1000];
+    char pattstr[1000];
+    char pnamestr[1000];
     int rc;
-    uint16_t millis = 100;
-    rgb_t rgb = {0,0,0};
-    uint8_t count = 1;
+    uint16_t millis = 0;
+    rgb_t rgb = {0,0,0}; // for parsecolor
+    uint8_t count = 0;
 
+    DictionaryCallbacks resultsdictc = DictionaryStandardStringCallbacks();
+    DictionaryRef resultsdict = DictionaryCreate( 100, &resultsdictc );
+    
     struct mg_str* uri = &hm->uri;
     struct mg_str* querystr = &hm->query_string;
 
     snprintf(uristr, uri->len+1, "%s", uri->p);
 
+    DictionaryInsert(resultsdict, "uri", uristr);
+    DictionaryInsert(resultsdict, "version", blink1_server_version);
+    
+    // parse all possible query args (it's just easier this way)
     if( mg_get_http_var(querystr, "millis", tmpstr, sizeof(tmpstr)) > 0 ) {
         millis = strtod(tmpstr,NULL);
     }
@@ -138,7 +187,6 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
     }
     if( mg_get_http_var(querystr, "rgb", tmpstr, sizeof(tmpstr)) > 0 ) {
         parsecolor( &rgb, tmpstr);
-        r = rgb.r; g = rgb.g; b = rgb.b;
     }
     if( mg_get_http_var(querystr, "count", tmpstr, sizeof(tmpstr)) > 0 ) {
         count = strtod(tmpstr,NULL);
@@ -149,7 +197,14 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
     if( mg_get_http_var(querystr, "bright", tmpstr, sizeof(tmpstr)) > 0 ) {
         bright = strtod(tmpstr,NULL);
     }
-
+    if( mg_get_http_var(querystr, "pattern", tmpstr, sizeof(tmpstr)) > 0 ) {
+        strcpy(pattstr, tmpstr);
+    }
+    if( mg_get_http_var(querystr, "pname", tmpstr, sizeof(tmpstr)) > 0 ) {
+        strcpy(pnamestr, tmpstr);
+    }
+    
+    // parse URI requests
     if( mg_vcmp( uri, "/") == 0 ) {
         sprintf(result, "Welcome to %s api server."
                 "All URIs start with '/blink1'. Supported URIs:\n", blink1_server_name);
@@ -161,42 +216,132 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
              mg_vcmp( uri, "/blink1/") == 0  ) {
         sprintf(result, "blink1 status");
     }
+    else if( mg_vcmp( uri, "/blink1/id") == 0 ||
+             mg_vcmp( uri, "/blink1/id/") == 0 ||
+             mg_vcmp( uri, "/blink1/enumerate") == 0  ) {
+        sprintf(result, "blink1 id");
+        int c = blink1_enumerate();
+
+        sprintf(tmpstr,"[");
+        for( int i=0; i< c; i++ ) {
+            sprintf(tmpstr, "%s\"%s\"", tmpstr,blink1_getCachedSerial(i));
+            if( i!=c-1 ) { sprintf(tmpstr, "%s,",tmpstr); } // ugh
+        }
+        sprintf(tmpstr,"%s]",tmpstr);
+        DictionaryInsert(resultsdict, "blink1_serialnums", tmpstr);
+        
+        const char* blink1_serialnum = blink1_getCachedSerial(0);
+        if( blink1_serialnum ) {
+            sprintf(tmpstr, "%s00000000", blink1_serialnum);
+            DictionaryInsert(resultsdict, "blink1_id", tmpstr);
+        }
+    }
     else if( mg_vcmp( uri, "/blink1/off") == 0 ) {
         sprintf(result, "blink1 off");
-        r = 0; g = 0; b = 0;
-        do_blink1_color();
+        rgb.r = 0; rgb.g = 0; rgb.b = 0;
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
     else if( mg_vcmp( uri, "/blink1/on") == 0 ) {
         sprintf(result, "blink1 on");
-        r = 255; g = 255; b = 255;
-        do_blink1_color();
+        rgb.r = 255; rgb.g = 255; rgb.b = 255;
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
     else if( mg_vcmp( uri, "/blink1/red") == 0 ) {
         sprintf(result, "blink1 red");
-        r = 255; g = 0; b = 0;
-        do_blink1_color();
+        rgb.r = 255; rgb.g = 0; rgb.b = 0;
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
     else if( mg_vcmp( uri, "/blink1/green") == 0 ) {
         sprintf(result, "blink1 green");
-        r = 0; g = 255; b = 0;
-        do_blink1_color();
+        rgb.r = 0; rgb.g = 255; rgb.b = 0;
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
     else if( mg_vcmp( uri, "/blink1/blue") == 0 ) {
-        sprintf(result, "blink1 on");
-        r = 0; g = 0; b = 255;
-        do_blink1_color();
+        sprintf(result, "blink1 blue");
+        rgb.r = 0; rgb.g = 0; rgb.b = 255;
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
     else if( mg_vcmp( uri, "/blink1/fadeToRGB") == 0 ) {
         sprintf(result, "blink1 fadeToRGB");
-        do_blink1_color();
+        blink1_do_color(rgb, millis, ledn, bright, result);
     }
-    else if( mg_vcmp( uri, "/blink1/blink") == 0 ) {
+    else if( mg_vcmp(uri, "/blink1/blink") == 0 ) {
         sprintf(result, "blink1 blink");
-        if( r==0 && g==0 && b==0 ) { r = 255; g = 255; b = 255; }
+        if( rgb.r==0 && rgb.g==0 && rgb.b==0 ) { rgb.r=255;rgb.g=255;rgb.b=255; }
+        if( count==0 ) { count = 3; }
+        if( millis==0 ) { millis = 300; }
+        int repeats = -1;
+        patternline_t pattern[32];
+        blink1_adjustBrightness( bright, &rgb.r, &rgb.g, &rgb.b);
+        sprintf(tmpstr, "%d,#%2.2x%2.2x%2.2x,%f,%d,#000000,%f,0",
+                count, rgb.r,rgb.g,rgb.b, (float)millis/1000.0, ledn,
+                (float)millis/1000.0);
+        msg("pattstr:%s\n", tmpstr);
+        int pattlen = parsePattern( tmpstr, &repeats, pattern);
+        
         blink1_device* dev = blink1_open();
-        blink1_adjustBrightness( bright, &r, &g, &b);
+        for( int i=0; i<pattlen; i++ ) {
+            patternline_t pat = pattern[i];
+            blink1_setLEDN(dev, pat.ledn);
+            msg("  writing line %d: %2.2x,%2.2x,%2.2x : %d : %d\n",
+                  i, pat.color.r,pat.color.g,pat.color.b, pat.millis, pat.ledn );
+            rc = blink1_writePatternLine(dev, pat.millis, pat.color.r, pat.color.g, pat.color.b, i);
+        }
+        rc = blink1_playloop(dev, 1, 0/*startpos*/, pattlen-1/*endpos*/, count/*count*/);
+        blink1_close(dev);
+    }
+    /*
+    else if( mg_vcmp(uri, "/blink1/pattern") == 0 ||
+             mg_vcmp(uri, "/blink1/pattern/") == 0 ) {
+
+        tmpstr
+        DictionaryInsert(resultsdict, "patterns", tmpstr);
+        
+    }
+    else if( mg_vcmp(uri, "/blink1/pattern/add") == 0 ) {
+        sprintf(result, "blink1 pattern add");
+        DictionaryInsert(patterndict, pnamestr, pattstr);
+        DictionaryInsert(resultsdict, "pattern", pattstr);
+    }
+    */
+    else if( mg_vcmp(uri, "/blink1/pattern/play") == 0 ) {
+        sprintf(result, "blink1 pattern play");
+        /*
+        if( pnamestr[0] != 0 && pattstr[0] != 0 ) { 
+            DictionaryInsert(patterndict, pnamestr, pattstr);
+        }
+        if( pnamestr[0] != 0 ) {
+            DictionaryGetValue(patterndict, pnamestr);
+        }
+        */
+        if( pattstr[0] == 0 ) { // no pattern
+        }
+        
+        patternline_t pattern[32];
+        int repeats = -1;
+        int pattlen = parsePattern( pattstr, &repeats, pattern);
+        if( !count ) { count = repeats; }
+        blink1_device* dev = blink1_open();
+        msg("pattlen:%d, repeats:%d\n", pattlen,repeats);
+        for( int i=0; i<pattlen; i++ ) {
+            patternline_t pat = pattern[i];
+            blink1_setLEDN(dev, pat.ledn);
+            msg("    writing line %d: %2.2x,%2.2x,%2.2x : %d : %d\n",
+                  i, pat.color.r,pat.color.g,pat.color.b, pat.millis, pat.ledn );
+            rc = blink1_writePatternLine(dev, pat.millis, pat.color.r, pat.color.g, pat.color.b, i);
+        }
+        rc = blink1_playloop(dev, 1, 0/*startpos*/, pattlen-1/*endpos*/, count/*count*/);
+        blink1_close(dev);
+    }
+    else if( mg_vcmp( uri, "/blink1/blinkserver") == 0 ) {
+        sprintf(result, "blink1 blink");
+        //if( r==0 && g==0 && b==0 ) { r = 255; g = 255; b = 255; }
+        if( millis==0 ) { millis = 200; }
+        blink1_device* dev = blink1_open();
+        //blink1_adjustBrightness( bright, &r, &g, &b);
+        //msg("rgb:%d,%d,%d\n",r,g,b);
         for( int i=0; i<count; i++ ) {
-            blink1_fadeToRGBN( dev, millis/2, r,g,b, ledn );
+            blink1_fadeToRGBN( dev, millis/2, rgb.r,rgb.g,rgb.b, ledn );
             blink1_sleep( millis/2 ); // fixme
             blink1_fadeToRGBN( dev, millis/2, 0,0,0, ledn );
             blink1_sleep( millis/2 ); // fixme
@@ -205,12 +350,13 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
     }
     else if( mg_vcmp( uri, "/blink1/random") == 0 ) {
         sprintf(result, "blink1 random");
+        if( millis==0 ) { millis = 200; }
         srand( time(NULL) * getpid() );
         blink1_device* dev = blink1_open();
         for( int i=0; i<count; i++ ) {
-            r = rand() % 255;
-            g = rand() % 255;
-            b = rand() % 255 ;
+            uint8_t r = rand() % 255;
+            uint8_t g = rand() % 255;
+            uint8_t b = rand() % 255 ;
             blink1_adjustBrightness( bright, &r, &g, &b);
             blink1_fadeToRGBN( dev, millis/2, r,g,b, ledn );
             blink1_sleep( millis/2 ); // fixme
@@ -223,30 +369,32 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
     }
 
     if( result[0] != '\0' ) {
-        sprintf(tmpstr, "#%2.2x%2.2x%2.2x", r,g,b );
+        sprintf(tmpstr, "#%2.2x%2.2x%2.2x", rgb.r,rgb.g,rgb.b );
         mg_printf(nc, "%s", "HTTP/1.1 200 OK\r\nTransfer-Encoding: chunked\r\n\r\n");
         mg_printf_http_chunk(nc,
-                             "{\n"
-                             "\"uri\":  \"%s\",\n"
-                             "\"result\":  \"%s\",\n"
-                             "\"millis\": \"%d\",\n"
+                             "{\n");
+
+        DictionaryPrintAsJsonMg(nc, resultsdict);
+        
+        mg_printf_http_chunk(nc,
+                             "\"millis\": %d,\n" 
                              "\"rgb\": \"%s\",\n"
-                             "\"bright\": \"%d\",\n"
-                             "\"ledn\": \"%d\",\n"
-                             "\"count\": \"%d\",\n"
-                             "\"millis\": \"%d\",\n"
-                             "\"version\": \"%s\"\n"
-                             "}\n",
-                             uristr,
-                             result,
+                             "\"ledn\": %d,\n"
+                             "\"bright\": %d,\n"
+                             "\"count\": %d,\n"
+                             "\"status\":  \"%s\"\n",
                              millis,
                              tmpstr,
-                             bright,
                              ledn,
+                             bright,
                              count,
-                             millis,
-                             blink1_server_version
+                             result
                              );
+
+        mg_printf_http_chunk(nc,
+                             "}\n"
+                             );
+        
         mg_send_http_chunk(nc, "", 0); /* Send empty chunk, the end of response */
     }
 
@@ -262,6 +410,9 @@ int main(int argc, char *argv[]) {
 
     mg_mgr_init(&mgr, NULL);
 
+    patterndictc = DictionaryStandardStringCallbacks();
+    patterndict = DictionaryCreate( 100, &patterndictc );
+        
     // parse options
     int option_index = 0, opt;
     char* opt_str = "qvhp:";
